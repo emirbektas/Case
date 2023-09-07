073aaa2c6805022cb40bc7b3a394f9d2bd646864
emirbkts <emr.bkts@hotmail.com>
2023-09-07 11:53:21 +0300

    structure

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import Body from "../layout/Body";
+import Footer from "../layout/Footer";
+import Header from "../layout/Header";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;

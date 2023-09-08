9ee02a52c8dd2a2e0bb03c3007fcd8465d802dc7
emirbkts <emr.bkts@hotmail.com>
2023-09-08 10:29:29 +0300

    api

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -3,9 +3,6 @@ import { useEffect } from "react";
 function Movies() {
   //   const [movies, setMovies] = useState([]);
 
-  //   const url =
-  //     "https://api.themoviedb.org//3/discover/movie?api_key=2718329d450e46aabda8f0221fe92a73";
-
   const apiKey = import.meta.env.VITE_REACT_APP_API_KEY;
 
   const getMovies = () => {
